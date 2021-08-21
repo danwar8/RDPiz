56087b3720bd35718cca511f1099ecd85bfbfb8d
danwar8 <86696262+danwar8@users.noreply.github.com>
2021-08-22 04:46:19 +0700

    Update NGROK-Setup.bat

--- NGROK-Setup.bat
+++ NGROK-Setup.bat
@@ -43,14 +43,14 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user runneradmin %_Password%
-echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
+echo All done! Connect your VM using RDPiz. When RDPiz expired and VM shutdown, Re-run jobs to get a new RDPiz.
 sh ip.sh
 echo User: runneradmin
 echo Pass: %_Password%
-curl -O https://raw.githubusercontent.com/danwar8/RDP/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/danwar8/RDP/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Everything.exe" https://raw.githubusercontent.com/danwar8/RDP/main/Files/Everything.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Windows-User.bat" https://raw.githubusercontent.com/danwar8/RDP/main/Files/Windows-User.bat > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/danwar8/RDPiz/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/danwar8/RDPiz/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Everything.exe" https://raw.githubusercontent.com/danwar8/RDPiz/main/Files/Everything.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Windows-User.bat" https://raw.githubusercontent.com/danwar8/RDPiz/main/Files/Windows-User.bat > out.txt 2>&1
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
